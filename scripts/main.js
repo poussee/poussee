49963103a9b14732215894fc1c11e9f292b67e77
Ruan <ruanqueirozl@hotmail.com>
2021-04-08 09:43:41 -0300

    Ajuste home

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,4 @@
-/**
- *
- *  sticky navigation
- *
- */
-
-let navbar = $(".navbar");
-
-$(window).scroll(function () {
-  // get the complete hight of window
-  let oTop = $(".buy").offset().top - window.innerHeight;
-  if ($(window).scrollTop() > oTop) {
-    navbar.addClass("sticky");
-  } else {
-    navbar.removeClass("sticky");
-  }
-});
-
- // Carousel marcas
+// Carousel marcas
 
  $(document).ready(function(){
   $('.logomarcas').slick({
@@ -53,68 +35,63 @@ $(window).scroll(function () {
   });
 });
 
-// Carousel depoimentos
+// Carousel montadoras
 
-$(document).ready(function(){
-  $('.depoimentos').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: '<i class="left-arrow"></i>',
-  nextArrow: '<i class="right-arrow"></i>',
-  dots: true,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  responsive: 
-  [
-  {
-     breakpoint: 600,
-     settings: {
-         infinite: true,
-         arrows: true,
-         slidesToShow: 1
-     }
-  },
-  {
-     breakpoint: 480,
-     settings: {
-         infinite: true,
-         arrows: true,
-         slidesToShow: 1
-     }
-  }
-  ]
+$(document).ready(function() {
+  $(".montadoras").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa fa-chevron-left"></i>',
+    nextArrow: '<i class="fa fa-chevron-right"></i>',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          infinite: true,
+          arrows: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
 
-// Carousel depoimentos reconhece
+// Carousel solucoes
 
-$(document).ready(function(){
-  $('.depo-reconhece').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: '<i class="left-arrow"></i>',
-  nextArrow: '<i class="right-arrow"></i>',
-  dots: true,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  responsive: 
-  [
-  {
-     breakpoint: 600,
-     settings: {
-         infinite: true,
-         arrows: true,
-         slidesToShow: 1
-     }
-  },
-  {
-     breakpoint: 480,
-     settings: {
-         infinite: true,
-         arrows: true,
-         slidesToShow: 1
-     }
-  }
-  ]
+$(document).ready(function() {
+  $(".sistemas-boxes").slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    nextArrow: '<i class="fa fa-chevron-right"></i>',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          infinite: true,
+          arrows: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
