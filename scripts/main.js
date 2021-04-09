d3fe2d31bd93a85138d0705a9519c36b2b86ca87
Ruan <ruanqueirozl@hotmail.com>
2021-04-09 12:12:38 -0300

    routes pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,3 @@
-// Carousel marcas
-
- $(document).ready(function(){
-  $('.logomarcas').slick({
-  slidesToShow: 5,
-  slidesToScroll: 5,
-  prevArrow: '<i class="left-arrow"></i>',
-  nextArrow: '<i class="right-arrow"></i>',
-  dots: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  responsive: 
-  [
-  {
-     breakpoint: 600,
-     settings: {
-         infinite: true,
-         arrows: true,
-         dots: false,
-         slidesToShow: 2,
-         slidesToScroll: 2
-     }
-  },
-  {
-     breakpoint: 480,
-     settings: {
-         infinite: true,
-         arrows: true,
-         dots: false,
-         slidesToShow: 1,
-         slidesToScroll: 1
-     }
-  }
-  ]
-  });
-});
 
 // Carousel montadoras
 
@@ -72,7 +36,7 @@ $(document).ready(function() {
   $(".sistemas-boxes").slick({
     slidesToShow: 2,
     slidesToScroll: 2,
-    nextArrow: '<i class="fa fa-chevron-right"></i>',
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 2000,
     responsive: [
