09b9f9d052626f71e480f9ca412884e2eee300b2
gabrielpanegrossi <gabriel7751535@gmail.com>
2021-07-30 18:12:03 -0300

    ajustes em toda a versão desktop

--- scripts/casesCard.js
+++ scripts/casesCard.js
@@ -1,10 +1,12 @@
-const cardsContainer = document.querySelector('.cases-row');
-const b2bButton = document.querySelector('.b2b-buttom');
-const b2cButton = document.querySelector('.b2c-buttom');
+const cardsContainer = document.querySelector('#cases-row');
+// const b2bButton = document.querySelector('.b2b-buttom');
+// const b2cButton = document.querySelector('.b2c-buttom');
+const moreCasesButton = document.querySelector('.moreCases');
 
-b2cButton.classList.toggle('segment-filter-on');
+// b2cButton.classList.toggle('segment-filter-on');
 
-activeFilter = 'b2b';
+let activeFilter = 'b2b';
+let cardsToShow = 3;
 
 const cases = [
 	{
@@ -13,7 +15,7 @@ const cases = [
 		textos: [
 			'Como aumentamos em',
 			'185%',
-			'o número de usuários de um e-commerce B2B ma Black Friday.',
+			'o número de usuários de um e-commerce B2B na Black Friday.',
 		],
 		link: './case-distribuidora.html',
 		type: 'B2B',
@@ -22,7 +24,7 @@ const cases = [
 		img: '../assets/img/cases/icone-contactCenter.png',
 		title: 'CONTACT CENTER',
 		textos: [
-			'Como ajudamos a criar presença digital de empresa de telecomunicações em',
+			'Como ajudamos a criar presença digital de uma empresa de telecomunicações em',
 			'menos de 6 meses.',
 		],
 		link: './case-contact-center.html',
@@ -38,36 +40,64 @@ const cases = [
 		link: './case-contabil.html',
 		type: 'B2B',
 	},
+	{
+		img: '../assets/img/cases/icone-mascara.png',
+		title: 'PRODUTOS HOSPITALARES',
+		textos: [
+			'Veja como a nossa estratégia de Google Ads fez os',
+			'leads em vendas',
+			'de máscara no B2B subir de',
+			'0 para 831',
+		],
+		link: './case-mascaras.html',
+		type: 'B2B',
+	},
 ];
 
-b2bButton.addEventListener('click', (e) => {
-	if (activeFilter != 'b2b') {
-		b2bButton.classList.toggle('segment-filter-on');
-		b2cButton.classList.toggle('segment-filter-on');
-		activeFilter = 'b2b';
-	}
-});
+// b2bButton.addEventListener('click', (e) => {
+// 	if (activeFilter != 'b2b') {
+// 		b2bButton.classList.toggle('segment-filter-on');
+// 		b2cButton.classList.toggle('segment-filter-on');
+// 		activeFilter = 'b2b';
+// 	}
+// });
 
-b2cButton.addEventListener('click', (e) => {
-	if (activeFilter != 'b2c') {
-		b2bButton.classList.toggle('segment-filter-on');
-		b2cButton.classList.toggle('segment-filter-on');
-		activeFilter = 'b2c';
-	}
-});
+// b2cButton.addEventListener('click', (e) => {
+// 	if (activeFilter != 'b2c') {
+// 		b2bButton.classList.toggle('segment-filter-on');
+// 		b2cButton.classList.toggle('segment-filter-on');
+// 		activeFilter = 'b2c';
+// 	}
+// });
 
 function insertCases() {
 	let cards = '';
-	let c = 1;
-
+	let createdCardsCounter = 0;
+	let strongCounter = 1;
+	let position = 1;
 	cases.forEach((item) => {
-		c = 1;
-		if (cards) {
+		if (createdCardsCounter < cardsToShow) {
+			strongCounter = 1;
+			if (position === 1) {
+				cards += `
+			<div
+			class="row cardRow"
+			style="
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				width:100%;
+			"
+		>            `;
+			}
+
+			if (position > 1) {
+				cards += `
+	<div class="col-md-1 col-12"></div>
+	`;
+			}
+
 			cards += `
-            <div class="col-md-1 col-12"></div>
-            `;
-		}
-		cards += `
         <div
         class="col-md-3 col-12 case-card"
         style="
@@ -83,31 +113,56 @@ function insertCases() {
         <p>
         `;
 
-		item.textos.forEach((texto) => {
-			if (c % 2 != 0) {
-				cards += `
+			item.textos.forEach((texto) => {
+				if (strongCounter % 2 != 0) {
+					cards += `
                 ${texto}
                 `;
-			} else {
-				cards += `<strong>
+				} else {
+					cards += `<strong>
                 ${texto}
                 </strong>`;
-			}
+				}
 
-			c++;
-			console.log(c % 2);
-		});
+				strongCounter++;
+			});
 
-		cards += `
+			cards += `
         </p>
         <a href="${item.link}">
             <img src="../assets/img/cases/cta-confira.png" alt="" />
         </a>
     </div>
 `;
+			if (position % 3 === 0) {
+				cards += `
+	</div>
+	`;
+				position = 0;
+			}
+			position++;
+			createdCardsCounter++;
+		}
 	});
+
+	if (cases.length % 3 != 0) {
+		cards += `
+		</div>
+		`;
+	}
 
 	cardsContainer.innerHTML = cards;
 }
 
 insertCases();
+
+function showMoreCards() {
+	if (cardsToShow < cases.length) {
+		cardsToShow += 3;
+		insertCases();
+	}
+}
+
+moreCasesButton.addEventListener('click', (e) => {
+	showMoreCards();
+});
--- scripts/main.js
+++ scripts/main.js
@@ -8,62 +8,62 @@ ScrollReveal().reveal('.img-brands', { delay: 500 });
 
 // Carousel montadoras
 
-$(document).ready(function() {
-  $(".montadoras").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: '<i class="left-arrow"></i>',
-    nextArrow: '<i class="right-arrow"></i>',
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 600,
-        settings: {
-          infinite: true,
-          arrows: false,
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          infinite: true,
-          arrows: false,
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+$(document).ready(function () {
+	$('.montadoras').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: '<i class="left-arrow"></i>',
+		nextArrow: '<i class="right-arrow"></i>',
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+			{
+				breakpoint: 600,
+				settings: {
+					infinite: true,
+					arrows: false,
+					slidesToShow: 1,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					infinite: true,
+					arrows: false,
+					slidesToShow: 1,
+				},
+			},
+		],
+	});
 });
 
 // Carousel solucoes
 
-$(document).ready(function() {
-  $(".sistemas-boxes").slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 600,
-        settings: {
-          infinite: true,
-          arrows: false,
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          infinite: true,
-          arrows: false,
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+$(document).ready(function () {
+	$('.sistemas-boxes').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		responsive: [
+			{
+				breakpoint: 600,
+				settings: {
+					infinite: true,
+					arrows: false,
+					slidesToShow: 1,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					infinite: true,
+					arrows: false,
+					slidesToShow: 1,
+				},
+			},
+		],
+	});
 });
